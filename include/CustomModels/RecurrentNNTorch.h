de28d6ffba9655b9643f4935735cb7410d29e3b5
Zhymir Thompson <dzhymir@gmail.com>
2023-08-24 01:27:46 -0400

    Added RNN model class.

---
+++ include/CustomModels/RecurrentNNTorch.h
@@ -0,0 +1,20 @@
+//
+// Created by zhymi on 5/24/2023.
+//
+
+#ifndef ONLINERNN_RECURRENTNNTORCH_H
+#define ONLINERNN_RECURRENTNNTORCH_H
+//#include <torch/script.h> // One-stop header.
+#include <torch/torch.h>
+#include <tuple>
+class RecurrentNeuralNetworkTorch : public torch::nn::Module {
+public:
+    virtual ~RecurrentNeuralNetworkTorch() {}
+//    virtual std::vector<torch::Tensor> parameters(bool recurse=true) = 0;
+//    virtual torch::OrderedDict<std::string, torch::Tensor> named_parameters(bool recurse = true) = 0;
+//    virtual torch::OrderedDict<std::string, torch::Tensor> named_buffers(bool recurse = true) = 0;
+    virtual std::tuple<torch::Tensor, torch::Tensor> predict(torch::Tensor inputVal, torch::Tensor hiddenState) = 0;
+    virtual torch::Tensor loss(torch::Tensor prediction, torch::Tensor actual) = 0;
+    virtual torch::Tensor makeHiddenState() = 0;
+};
+#endif //ONLINERNN_RECURRENTNNTORCH_H
---
+++ src/CustomModels/RecurrentNNTorch.cpp
@@ -0,0 +1,4 @@
+//
+// Created by zhymi on 5/23/2023.
+//
+#include "RecurrentNNTorch.h"
