6d312ebbac89917b4cc3d6079b66829788a69695
Zhymir Thompson <dzhymir@gmail.com>
2023-10-10 13:37:18 -0400

    Cleanup, added typedef.

--- include/CustomModels/RecurrentNNTorch.h
+++ include/CustomModels/RecurrentNNTorch.h
@@ -8,13 +8,12 @@
 #include <torch/torch.h>
 #pragma warning(pop)
 
+typedef std::tuple<torch::Tensor, torch::Tensor> inferenceTuple;
+
 class RecurrentNeuralNetworkTorch : public torch::nn::Module {
 public:
     virtual ~RecurrentNeuralNetworkTorch() {}
-//    virtual std::vector<torch::Tensor> parameters(bool recurse=true) = 0;
-//    virtual torch::OrderedDict<std::string, torch::Tensor> named_parameters(bool recurse = true) = 0;
-//    virtual torch::OrderedDict<std::string, torch::Tensor> named_buffers(bool recurse = true) = 0;
-    virtual std::tuple<torch::Tensor, torch::Tensor> predict(torch::Tensor inputVal, torch::Tensor hiddenState) = 0;
+    virtual inferenceTuple predict(torch::Tensor inputVal, torch::Tensor hiddenState) = 0;
     virtual torch::Tensor loss(torch::Tensor prediction, torch::Tensor actual) = 0;
     virtual torch::Tensor makeHiddenState(int batched=0, bool bidirect=false) = 0;
 };
